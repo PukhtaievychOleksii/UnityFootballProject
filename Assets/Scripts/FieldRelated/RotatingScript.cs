45542b24ab536d6f81677879a5e24be12250973c
Alex <pukhtaievych.oleksii@gmail.com>
2021-02-19 21:13:03 +0200

    SpaceBackground

---
+++ Assets/Scripts/FieldRelated/RotatingScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingScript : MonoBehaviour
+{
+    Transform transform;
+    private const float AngleToAdd = 1f / 60;
+    public float XRotatingCoef = 0f;
+    public float YRotatingCoef = 0f;
+    public float ZRotatingCoef = 0f;
+    public float RotationSpeedAmplifier = 1;
+    public bool EnableXRotation;
+    public bool EnableZRotation;
+    public bool EnableYRotation;
+    public 
+
+    void Start()
+    {
+      transform = GetComponent<Transform>();
+      
+      if(EnableXRotation && XRotatingCoef == 0)  XRotatingCoef = Random.Range(-1,1);
+      if(EnableYRotation && YRotatingCoef == 0)  YRotatingCoef = Random.Range(-1,1);
+      if(EnableZRotation && ZRotatingCoef == 0)  ZRotatingCoef = Random.Range(-1,1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float NewXRotation = transform.rotation.eulerAngles.x + XRotatingCoef * AngleToAdd * RotationSpeedAmplifier;
+        float NewYRotation = transform.rotation.eulerAngles.y + YRotatingCoef * AngleToAdd * RotationSpeedAmplifier;
+        float NewZRotation = transform.rotation.eulerAngles.z + ZRotatingCoef * AngleToAdd * RotationSpeedAmplifier;
+        transform.rotation = Quaternion.Euler(NewXRotation, NewYRotation, NewZRotation);
+        
+         
+            
+    }
+}
